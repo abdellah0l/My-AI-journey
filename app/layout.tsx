41aa6a93c4a604720650d8e11d57c5cd508944eb
abdellahing <abdallah.maamra@etu.univ-batna2.dz>
2025-09-15 23:00:57 +0100

    add Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script data-collect-dnt="true" async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+        <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true" alt="" referrerpolicy="no-referrer-when-downgrade"/></noscript>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
